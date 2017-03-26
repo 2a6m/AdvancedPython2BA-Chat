1c608aaaa2aafe44a96d8672c5fbd1472fa5d4ef
2a6m <maxime.bourguignon1996@gmail.com>
2017-03-26 18:18:30 +0200

    creation class client

--- Chat-client.py
+++ Chat-client.py
@@ -1,31 +1,40 @@
 import socket
+import threading
 
+class Client():
+    def __init__(self, host):
+        self._host = host
+        self._port = 5000
+        self._socket = socket.socket()
+        self._running = True
 
-def send_msg(s, smsg):
-    msg = smsg.encode()
-    totalsent = 0
-    while totalsent < len(msg):
-        sent = s.send(msg[totalsent:])
-        totalsent += sent
+    def send_msg(self, txt):
+        msg = txt.encode()
+        totalsent = 0
+        while totalsent < len(msg):
+            sent = self._socket.send(msg[totalsent:])
+            totalsent += sent
 
+    def listen(self):
+        while self._running:
+            data = self._socket.recv(4096).decode()
+            if len(data) != 0:
+                print('Reçu', len(data), 'octets :')
+                print(data)
 
-def rcv_msg(s):
-    data = s.recv(4096).decode()
-    print('Reçu', len(data), 'octets :')
-    print(data)
-
-
-def run():
-    rcv_msg(s)
-    ask = input()
-    if ask == 'Q':
+    def run(self):
+        self._socket.connect((self._host, self._port))
+        threading.Thread(target=self.listen).start()
+        while self._running:
+            ask = input()
+            if ask == 'Q':
+                self._running = False
+            else:
+                self.send_msg(ask)
+        self._socket.close()
         quit()
-    else:
-        send_msg(s, ask)
-    run()
 
-s = socket.socket()
-ip = str(input('adresse IP:',))
-port = int(input('n° port:',))
-s.connect((ip, port))
-run()
+if __name__ == "__main__":
+    h = str(input('address to connect:',))
+    Client(host=h).run()
+
