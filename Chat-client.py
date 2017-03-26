82b0810bf017d91d27bd8b5ce295cfb439f594ca
2a6m <maxime.bourguignon1996@gmail.com>
2017-03-26 19:11:18 +0200

    new interface: interface with command line

--- Chat-client.py
+++ Chat-client.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import sys
+
 
 class Client():
     def __init__(self, host, name):
@@ -7,9 +9,8 @@ class Client():
         self._host = host
         self._port = 5000
         self._socket = socket.socket()
-        self._running = True
 
-    def send_msg(self, txt):
+    def _send(self, txt):
         msg = self._name + " - " + txt
         msg = msg.encode()
         totalsent = 0
@@ -17,24 +18,36 @@ class Client():
             sent = self._socket.send(msg[totalsent:])
             totalsent += sent
 
-    def listen(self):
+    def _listen(self):  #quand on exit(), il attend encore un message avant de quitter
         while self._running:
             data = self._socket.recv(4096).decode()
             if len(data) != 0:
-                print('Reçu', len(data), 'octets :')
+                #print('Reçu', len(data), 'octets :')
                 print(data)
 
     def run(self):
+        handlers = {
+            '/exit': self._exit,
+            '/send': self._send
+        }
+        self._running = True
         self._socket.connect((self._host, self._port))
-        threading.Thread(target=self.listen).start()
+        threading.Thread(target=self._listen).start()
         while self._running:
-            ask = input()
-            if ask == 'Q':
-                self._running = False
-            else:
-                self.send_msg(ask)
+            line = sys.stdin.readline().rstrip() + ' '
+            # Extract the command and the param
+            command = line[:line.index(' ')]
+            param = line[line.index(' ') + 1:].rstrip()
+            # Call the command handler
+            if command in handlers:
+                try:
+                    handlers[command]() if param == '' else handlers[command](param)
+                except:
+                    print("Erreur lors de l'exécution de la commande.")
+
+    def _exit(self):    #erreur à cause du thread, il faut arrêter le thread
+        self._running = False
         self._socket.close()
-        quit()
 
 if __name__ == "__main__":
     h = str(input('address to connect:',))
