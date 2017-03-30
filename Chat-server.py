68ba0d09c6da9801846beb88e022fb16302dd4e5
2a6m <maxime.bourguignon1996@gmail.com>
2017-03-30 20:53:08 +0200

    rename constant

--- Chat-server.py
+++ Chat-server.py
@@ -6,12 +6,12 @@ import json
 
 class Server:
     def __init__(self):
-        self._host = ''
-        self._port = 5000
-        self._sock = socket.socket()
-        self._sock.bind((self._host, self._port))
-        self._clients = {}
-        self._running = True
+        self.__host = ''
+        self.__port = 5000
+        self.__sock = socket.socket()
+        self.__sock.bind((self.__host, self.__port))
+        self.__clients = {}
+        self.__running = True
 
     def _send(self, txt, client):
         msg = txt.encode()
@@ -21,21 +21,21 @@ class Server:
             totalsent += sent
 
     def nameForbidden(self):
-        lst = [self._clients[cl] for cl in self._clients]
+        lst = [self.__clients[cl] for cl in self.__clients]
         name_lst = [elem[0] for elem in lst] + ['']
         name = {'name forbidden': name_lst}
         return(json.dumps(name))
 
-    def listen(self):
-        self._sock.listen()
+    def run(self):
+        self.__sock.listen()
         print('listenning...')
-        while self._running:
-            client, addr = self._sock.accept()
+        while self.__running:
+            client, addr = self.__sock.accept()
             client.send(self.nameForbidden().encode())
             name_client = client.recv(2048).decode()
             pp_client = (client.recv(2048).decode(), int(client.recv(2048).decode()))
-            self._clients[client] = (name_client, addr, pp_client)
-            print(self._clients)
+            self.__clients[client] = (name_client, addr, pp_client)
+            print(self.__clients)
             client.send("#senda You're connected to the server".encode())
             threading.Thread(target=self.listenToClient, args=(client, addr)).start()
 
@@ -66,35 +66,31 @@ class Server:
             self.sendToExpeditor('You send a wrong message', client, '')
 
     def sendToAll(self, msg, client, addr):
-        data = self._clients[client][0] + ' - ' + msg
+        data = self.__clients[client][0] + ' - ' + msg
         print(addr, " : ", data)
         data = '#senda ' + data
-        for cl in self._clients:
+        for cl in self.__clients:
             self._send(data, cl)
 
     def sendClients(self, msg, client, addr):
         data = {}
-        for cl in self._clients:
-            data[str(self._clients[cl][0])] = self._clients[cl][2]
+        for cl in self.__clients:
+            data[str(self.__clients[cl][0])] = self.__clients[cl][2]
         print(data)
         data = json.dumps(data)
         txt = '#clients ' + data
         self._send(txt, client)
 
     def listenToClient(self, client, addr):
-        while self._running:
+        while self.__running:
             try:
                 txt = client.recv(2048).decode()
             except:
-                self._clients.pop(client)
+                self.__clients.pop(client)
                 client.close()
                 return False
             order, msg = self.analyse(txt)
             self.treat(order, msg, client, addr)
 
 if __name__ == "__main__":
-    Server().listen()
-    ask = str(input('[Q]uit'))
-    if ask == 'Q':
-        Server._running = False
-        quit()
+    Server().run()
